77d453b1676f75ff5e44542c25774cbffad284fd
Emil Björklund <emil.bjorklund@inuse.se>
2016-12-14 20:57:04 +0100

    validate getMinDate and getMaxDate

--- src/ca11y/lib/validate.js
+++ src/ca11y/lib/validate.js
@@ -6,4 +6,12 @@ export default function validate(options) {
   if (!!options.formatter && typeof options.formatter !== 'function') {
     throw new TypeError(`Ca11y: "options.formatter" must be a function.`)
   }
+
+  if (!!options.getMinDate && typeof options.getMinDate !== 'function') {
+    throw new TypeError(`Ca11y: "options.getMinDate" must be a function.`)
+  }
+
+  if (!!options.getMaxDate && typeof options.getMaxDate !== 'function') {
+    throw new TypeError(`Ca11y: "options.getMinDate" must be a function.`)
+  }
 }
